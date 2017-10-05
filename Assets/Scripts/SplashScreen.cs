3b52e23203a68a285743cdcdb918fe0f8b36a3dd
NatashaHMP <natasham630@gmail.com>
2017-10-05 15:02:18 -0300

    Create script to splash screen

---
+++ Assets/Scripts/SplashScreen.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public sealed class SplashScreen : MonoBehaviour {
+
+	private int secondsToChange = 2;
+
+	// Use this for initialization
+	private void Start () {
+
+		StartCoroutine ("Countdown");
+	}
+
+	// wait some seconds to change scene
+	private IEnumerator Countdown () {
+		yield return new WaitForSeconds (secondsToChange);
+		SceneManager.LoadScene ("MainMenu");
+	}
+}
