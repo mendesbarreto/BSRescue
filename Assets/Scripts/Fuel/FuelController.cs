d3260b81e48de5a4ffb3be51e869ba493023d3ce
NatashaHMP <natasham630@gmail.com>
2017-10-11 15:31:54 -0300

    Refactoring fuel script

--- Assets/Scripts/Fuel/FuelController.cs
+++ Assets/Scripts/Fuel/FuelController.cs
@@ -2,20 +2,12 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class FuelController : MonoBehaviour {
-
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
-
-	private void OnTriggerEnter2D (Collider2D collider) {
-		if (collider.gameObject.tag == MainGame.instance.PlayerTag) {
+public class FuelController : MonoBehaviour 
+{
+	private void OnTriggerEnter2D (Collider2D collider) 
+	{
+		if (collider.gameObject.tag == Constants.instance.PlayerTagName) 
+		{
 			Destroy (gameObject);
 		}
 	}
