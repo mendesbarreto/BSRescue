1ba10009199bc4f5b72acff7cc8a33865d1725d4
NatashaHMP <natasham630@gmail.com>
2017-10-11 17:46:30 -0300

    Refactoring game controller

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -2,28 +2,192 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerController : MonoBehaviour {
+public class PlayerController : MonoBehaviour 
+{
+	[SerializeField]
+	private float gameGoal;
 
-	// Use this for initialization
-	void Start () {
-		
+	[SerializeField]
+	private float speedUp;
+
+	[SerializeField]
+	private float speedDown;
+
+	[SerializeField]
+	private float speedHorizontal;
+
+	[SerializeField]
+	private bool keyboard;
+
+	[SerializeField]
+	private Canvas winScreen;
+
+	public float GameGoal 
+	{
+		get { return gameGoal; }
+		set { gameGoal = value; }
 	}
-	
-	// Update is called once per frame
-	void Update () {
-		LimitPlayerOnScreen ();
+
+	public float SpeedUp 
+	{
+		get { return speedUp; }
+		set { speedUp = value; }
 	}
 
-	// Limit player on screen
-	private void LimitPlayerOnScreen () {
+	public float SpeedDown 
+	{
+		get { return speedDown; }
+		set { speedDown = value; }
+	}
 
-		var distanceZ = (transform.position - Camera.main.transform.position).z;
-		var topBorder = Camera.main.ViewportToWorldPoint (new Vector3 (0, 0, distanceZ)).y;
-		var bottonBorder = Camera.main.ViewportToWorldPoint (new Vector3 (0, 1, distanceZ)).y;
+	public float SpeedHorizontal 
+	{
+		get { return speedHorizontal; }
+		set { speedHorizontal = value; }
+	}
 
-		//impedindo que o player suba
-		transform.position = new Vector3 (transform.position.x,
-			Mathf.Clamp (transform.position.y, topBorder, bottonBorder),
-			transform.position.z);
+	public bool Keyboard 
+	{
+		get { return keyboard; }
+		set { keyboard = value; }
+	}
+
+	public Canvas WinScreen 
+	{
+		get { return winScreen; }
+		set { winScreen = value; }
+	}
+
+	private const float ZERO_TOUCH = 0;
+	private const string PLAY_KEY_KEYBOARD = "space";
+	private const float DIRECTION_UP = 1;
+	private const float DIRECTION_DOWN = -1;
+	private const float DIRECTION_MOVE_HORIZONTAL = 1;
+
+	private Rigidbody2D playerRb;
+	private Death death;
+
+	private float directionMoveVertical;
+	private bool pressKeyToPlay;
+	private bool gameStart;
+	private float amountTouch;
+
+	private void Start () 
+	{
+		LoadResources ();
+	}
+
+	private void Update () 
+	{
+		amountTouch = Input.touches.Length;
+
+		ChooseConsole ();
+
+		SetStartGame ();
+	}
+
+	private void FixedUpdate () 
+	{
+		CheckGameStart ();
+
+		CheckTouch ();
+	}
+
+	private void LoadResources () 
+	{
+		winScreen.enabled = false;
+		playerRb = GetComponent<Rigidbody2D> ();
+	}
+
+	// Choose keyboard or touch
+	private void ChooseConsole () 
+	{
+		if (!keyboard) 
+		{
+			CheckTouch ();
+		} 
+		else 
+		{
+			pressKeyToPlay = Input.GetKey (PLAY_KEY_KEYBOARD);
+		}
+	}
+
+	// check if player is touching
+	private void CheckTouch () 
+	{
+		if (amountTouch > ZERO_TOUCH) {
+			pressKeyToPlay = true;
+		} 
+		else 
+		{
+			pressKeyToPlay = false;
+		}
+	}
+
+	// Set Start Game
+	private void SetStartGame () 
+	{
+		if (pressKeyToPlay) {
+			gameStart = true;
+		}
+	}
+
+	// check if game start
+	private void CheckGameStart ()
+	{
+		if (gameStart) 
+		{
+			CheckGameGoal ();
+		}
+	}
+
+	// Check game goal
+	private void CheckGameGoal () 
+	{
+		if (playerRb.position.x < gameGoal) 
+		{
+			Move ();
+		} 
+		else 
+		{
+			winScreen.enabled = true;
+			StopPlayer ();
+		}
+	}
+
+	// Select direction of the player (up or down)
+	private void PlayerUpOrDown () 
+	{
+		if (pressKeyToPlay) 
+		{
+			directionMoveVertical = DIRECTION_UP;
+		} 
+		else 
+		{
+			directionMoveVertical = DIRECTION_DOWN;
+		}
+	}
+
+	// Player Move
+	private void Move () 
+	{
+		float moveToHorizontal = DIRECTION_MOVE_HORIZONTAL * speedHorizontal;
+
+		PlayerUpOrDown ();
+
+		if (directionMoveVertical == DIRECTION_UP) 
+		{
+			playerRb.velocity = new Vector2 (moveToHorizontal, directionMoveVertical * speedUp);
+		} 
+		else 
+		{
+			playerRb.velocity = new Vector2 (moveToHorizontal, directionMoveVertical * speedDown);
+		}
+	}
+
+	// Player Stop
+	private void StopPlayer () 
+	{
+		playerRb.velocity = Vector2.zero;
 	}
 }
