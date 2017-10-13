ea4a2f747423500865431b1e3ae393dee554a39b
NatashaHMP <natasham630@gmail.com>
2017-10-13 16:06:45 -0300

    Create script player game status

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -4,17 +4,6 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour 
 {
-	[SerializeField]
-	private Canvas winScreen;
-
-	public Canvas WinScreen 
-	{
-		get { return winScreen; }
-		set { winScreen = value; }
-	}
-
-	private const float ZERO_TOUCH = 0;
-	private const string PLAY_KEY_KEYBOARD = "space";
 	private const float DIRECTION_UP = 1;
 	private const float DIRECTION_DOWN = -1;
 	private const float DIRECTION_MOVE_HORIZONTAL = 1;
@@ -22,129 +11,49 @@ public class PlayerController : MonoBehaviour
 	private const float SPEED_DOWN_PLAYER = 1.5f;
 	private const float SPEED_HORIZONTAL_PLAYER = 3f;
 
-	private Rigidbody2D playerRb;
 	private Death death;
+	private PlayerGameStatus playerConfig;
+	private Rigidbody2D playerRb;
 
-	private float directionMoveVertical;
-	private bool pressKeyToPlay;
-	private bool gameStart;
-	private float amountTouch;
+	private float moveToHorizontal;
+	private float moveToVertical;
 
 	private void Start () 
 	{
 		LoadResources ();
 	}
 
-	private void Update () 
-	{
-		amountTouch = Input.touches.Length;
-
-		ChooseConsole ();
-
-		SetStartGame ();
-	}
-
-	private void FixedUpdate () 
-	{
-		CheckGameStart ();
-
-		CheckTouch ();
-	}
-
 	private void LoadResources () 
 	{
-		winScreen.enabled = false;
 		playerRb = GetComponent<Rigidbody2D> ();
-	}
-
-	// Choose keyboard or touch
-	private void ChooseConsole () 
-	{
-		if (!GameSystem.instance.Keyboard) 
-		{
-			CheckTouch ();
-		} 
-		else 
-		{
-			pressKeyToPlay = Input.GetKey (PLAY_KEY_KEYBOARD);
-		}
-	}
-
-	// check if player is touching
-	private void CheckTouch () 
-	{
-		if (amountTouch > ZERO_TOUCH) {
-			pressKeyToPlay = true;
-		} 
-		else 
-		{
-			pressKeyToPlay = false;
-		}
-	}
-
-	// Set Start Game
-	private void SetStartGame () 
-	{
-		if (pressKeyToPlay) {
-			gameStart = true;
-		}
-	}
-
-	// check if game start
-	private void CheckGameStart ()
-	{
-		if (gameStart) 
-		{
-			CheckGameGoal ();
-		}
-	}
-
-	// Check game goal
-	private void CheckGameGoal () 
-	{
-		if (playerRb.position.x < GameSystem.instance.GameGoal) 
-		{
-			Move ();
-		} 
-		else 
-		{
-			winScreen.enabled = true;
-			StopPlayer ();
-		}
+		playerConfig = GetComponent<PlayerGameStatus> ();
 	}
 
 	// Select direction of the player (up or down)
-	private void PlayerUpOrDown () 
+	private void DirectionVertical () 
 	{
-		if (pressKeyToPlay) 
+		if (playerConfig.PressKeyToPlay) 
 		{
-			directionMoveVertical = DIRECTION_UP;
+			moveToVertical = DIRECTION_UP * SPEED_UP_PLAYER;
 		} 
 		else 
 		{
-			directionMoveVertical = DIRECTION_DOWN;
+			moveToVertical = DIRECTION_DOWN * SPEED_DOWN_PLAYER;
 		}
 	}
 
 	// Player Move
-	private void Move () 
+	public void Move () 
 	{
-		float moveToHorizontal = DIRECTION_MOVE_HORIZONTAL * SPEED_HORIZONTAL_PLAYER;
+		moveToHorizontal = DIRECTION_MOVE_HORIZONTAL * SPEED_HORIZONTAL_PLAYER;
 
-		PlayerUpOrDown ();
+		DirectionVertical ();
 
-		if (directionMoveVertical == DIRECTION_UP) 
-		{
-			playerRb.velocity = new Vector2 (moveToHorizontal, directionMoveVertical * SPEED_UP_PLAYER);
-		} 
-		else 
-		{
-			playerRb.velocity = new Vector2 (moveToHorizontal, directionMoveVertical * SPEED_DOWN_PLAYER);
-		}
+		playerRb.velocity = new Vector2 (moveToHorizontal, moveToVertical);
 	}
 
 	// Player Stop
-	private void StopPlayer () 
+	public void StopPlayer () 
 	{
 		playerRb.velocity = Vector2.zero;
 	}
---
+++ Assets/Scripts/Player/PlayerGameStatus.cs
@@ -0,0 +1,127 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerGameStatus : MonoBehaviour 
+{
+	[SerializeField]
+	private float gameGoal = 15;
+
+	[SerializeField]
+	private bool isKeyboard;
+
+	[SerializeField]
+	private Canvas winScreen;
+
+	private bool pressKeyToPlay;
+
+	public float GameGoal 
+	{
+		get { return gameGoal; }
+		set { gameGoal = value; }
+	}
+
+	public bool IsKeyboard 
+	{
+		get { return isKeyboard; }
+		set { isKeyboard = value; }
+	}
+
+	public Canvas WinScreen 
+	{
+		get { return winScreen; }
+		set { winScreen = value; }
+	}
+
+	public bool PressKeyToPlay 
+	{
+		get { return pressKeyToPlay; }
+		set { pressKeyToPlay = value; }
+	}
+
+
+	private const string KEY_KEYBOARD = "space";
+	private const float ZERO_TOUCH = 0;
+
+	private float amountTouch;
+	private bool gameStart;
+
+	private PlayerController playerController;
+	private Rigidbody2D playerRb;
+
+	private void Awake ()
+	{
+		playerController = GetComponent<PlayerController> ();
+		winScreen.enabled = false;
+		playerRb = GetComponent<Rigidbody2D> ();
+	}
+
+	private void Update ()
+	{
+		amountTouch = Input.touches.Length;
+		ChooseConsole ();
+		SetStartGame ();
+	}
+
+	private void FixedUpdate ()
+	{
+		// First Check if Game Start
+		CheckGameStart ();
+	}
+
+	// Choose keyboard or touch
+	private void ChooseConsole () 
+	{
+		if (!isKeyboard) 
+		{
+			CheckTouch ();
+		} 
+		else 
+		{
+			pressKeyToPlay = Input.GetKey (KEY_KEYBOARD);
+		}
+	}
+
+	// check if game start
+	private void CheckGameStart ()
+	{
+		if (gameStart) 
+		{
+			CheckGameGoal ();
+		}
+	}
+
+	private void CheckTouch () 
+	{
+		if (amountTouch > ZERO_TOUCH) {
+			pressKeyToPlay = true;
+		} 
+		else 
+		{
+			pressKeyToPlay = false;
+		}
+	}
+
+	// Set Start Game
+	private void SetStartGame () 
+	{
+		if (pressKeyToPlay) {
+			gameStart = true;
+		}
+	}
+
+	// Check game goal
+	private void CheckGameGoal () 
+	{
+		if (playerRb.position.x < gameGoal) 
+		{
+			playerController.Move ();
+		} 
+		else 
+		{
+			winScreen.enabled = true;
+			playerController.StopPlayer ();
+		}
+	}
+
+}
