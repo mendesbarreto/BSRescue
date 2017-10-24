e1218187bc40017a1b349f99ff266ad2ce860ea7
NatashaHMP <natasham630@gmail.com>
2017-10-24 17:26:27 -0200

    Update scenes

---
+++ Assets/Scripts/SpecialEffect.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public sealed class SpecialEffect : MonoBehaviour {
+
+    public static SpecialEffect Instance;
+
+    [SerializeField]
+    private ParticleSystem brokeEffect;
+
+
+    private void Awake()
+    {
+        if (Instance != null)
+        {
+            Debug.LogError("Multiple instances of SpecialEffectsHelper!");
+        }
+
+        Instance = this;
+    }
+
+
+    public void Explosion(Vector2 position)
+    {
+        instantiate(brokeEffect, position);
+    }
+
+    private ParticleSystem instantiate(ParticleSystem prefab, Vector2 position)
+    {
+        ParticleSystem newParticleSystem = Instantiate(prefab, position, Quaternion.identity) as ParticleSystem;
+        Destroy(newParticleSystem.gameObject, newParticleSystem.main.startLifetimeMultiplier);
+        return newParticleSystem;
+    }
+}
